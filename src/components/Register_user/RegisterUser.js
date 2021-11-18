85f7196f1cba30a310225661979fe35eedc793ef
camilosaenz <48153830+camilosaenz@users.noreply.github.com>
2021-11-17 23:41:06 -0500

    Register and update Client

--- src/components/Register_user/RegisterUser.js
+++ src/components/Register_user/RegisterUser.js
@@ -10,7 +10,7 @@ import Box from '@material-ui/core/Box';
 class RegisterUser extends Component {
   constructor(props) {
     super(props);
-    this.state = { url: 'https://ezbrowser.herokuapp.com/', confirmReister: true, user: '', email: '', university: '', address: '', number: '', password: '', confirmPassword: '' }
+    this.state = { url: 'https://ezbrowser.herokuapp.com/', confirmReister: true, user: '', email: '', username: '', address: '', number: '', password: '', confirmPassword: '' }
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   render() {
@@ -33,7 +33,7 @@ class RegisterUser extends Component {
                 <TextField id="email" name="email" label="Correo Electrónico" required value={this.state.email} onChange={this.handleAll} autoComplete="email" autoFocus />
               </FormControl>
               <FormControl margin="normal" required fullWidth>
-                <TextField id="university" name="university" label="Barrio" value={this.state.university} required onChange={this.handleAll} autoComplete="university" autoFocus />
+                <TextField id="username" name="username" label="Username" value={this.state.username} required onChange={this.handleAll} autoComplete="username" autoFocus />
               </FormControl>
               <FormControl margin="normal" required fullWidth>
                 <TextField name="address" id="address" required label="Dirección" value={this.state.address} onChange={this.handleAll} autoComplete="current-password" />
@@ -79,7 +79,7 @@ class RegisterUser extends Component {
     var f = "@";
     console.log(this.state.email);
     e.preventDefault();
-    if (!this.state.email || !this.state.user || !this.state.university || !this.state.address || !this.state.confirmPassword || !this.state.password || !this.state.number) {
+    if (!this.state.email || !this.state.user || !this.state.username || !this.state.address || !this.state.confirmPassword || !this.state.password || !this.state.number) {
       Swal.fire("Faltó ingresar un dato", "Por favor ingrese todos los datos", "error");
     } else if (!this.state.email.includes(f)) {
       Swal.fire("Correo Electrónico ingresado erróneamente.", "Por favor ingrese un Correo Electrónico válido.", "error");
@@ -87,10 +87,10 @@ class RegisterUser extends Component {
       Swal.fire("Las contraseñas ingresadas no coinciden.", "Por favor ingrese de nuevo las contraseñas y verifique que sean las mismas.", "error");
     } else {
       await axios.post(this.state.url + 'auth/addUser', {
-        username: this.state.user,
+        name: this.state.user,
         nombreCompleto: this.state.user,
         email: this.state.email,
-        barrio: this.state.university,
+        username: this.state.username,
         password: this.state.password,
         direccionResidencia: this.state.address,
         numero: this.state.number,
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,76 +7,84 @@ import secure from './secure.png';
 import money from './money.png';
 import product from './product.png';
 import Grid from '@material-ui/core/Grid';
+import { TextField, MenuItem } from '@material-ui/core';     
 
 class Home extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
 
-    render() {
-        document.body.classList.add('home');
-        return (
-          <div >
-            <section id="home" className="home">
-              <div id="discover">
-                <img id="discoverimg" src={browse} alt="discover quickmobility"/>
-                <header className="home">
-                  <div>
-                  <h1 className="home"> Ez Browser 🔍 </h1>
-                  </div>
-                    <div className="header-right">
-                     <a href="/login">Ingresar</a>
-                     <a href="/RegisterUser">Registro</a>
-                     <a href="#about">Acerca de</a>
-                   </div>
-                </header>
-                <div id="slogan" className="centered">
-                    <p>La aplicación que nadie pidió, pero que todos necesitan.</p>
+  render() {
+    document.body.classList.add('home');
+    return ( 
+      <div >
+        <section id="home" className="home">
+          <div id="discover">
+            <img id="discoverimg" src={browse} alt="discover quickmobility" />
+            <header className="home">
+              <div>
+                <h1 className="home"> Ez Browser 🔍 </h1>
+              </div>
+              <div className="header-right">
+                <a href="/login">Ingresar</a>
+                <div>
+                  
+                  <TextField variant="outlined" label="Registrate" select required fullWidth
+                    onChange={this.handleRolChange}>
+                    <MenuItem value="cliente"><a href="/RegisterUser">Registro Vendedor</a></MenuItem>
+                    <MenuItem value="vendedor"><a href="/RegisterClient">Registro Cliente</a></MenuItem>
+                  </TextField>
                 </div>
-                  </div>
-              </section>
-              <section id="about" className="home">
-                <div id="info" className="gridcont">
-                   <Grid container spacing={3}>
-                     <Grid item xs={6}>
-                        <h2 className="home"> ¿Quienes somos? </h2>
-                       <p>Somos una plataforma web donde los usuarios pueden buscar cualquier tipo de producto que estén interesados en comprar. La plataforma recopila los productos buscados por el usuario en todas las tiendas, comparando precios y mostrando la mejor opción con el mejor precio, ahorrándole tiempo al cliente de tener que recurrir a buscar página por página mirando precios, Ez Browser lo hace automáticamente para tener una mejor Experiencia en compras.</p>
-                     </Grid>
-                     <Grid item xs={6} className="cuadro">
-                       <img src={search} alt="cuadro quickmobility"/>
-                     </Grid>
-                 </Grid>
-                </div>
-              </section>
-              <section id="ls" className="home">
-                  <div id="otros" className="gridcont">
-                     <Grid container spacing={3}>
-                       <Grid item xs>
-                          <h3 className="home"> Busca de forma más segura </h3>
-                          <p> Podrás buscar tu producto sin necesidad de ingresar a distintas páginas web inseguras. </p>
-                          <img src={secure} className="valores" alt="container descripcion"/>
-                       </Grid>
-                       <Grid item xs>
-                          <h3 className="home"> Ahorra Dinero </h3>
-                          <p> Pagarás un valor más económico en comparación a otras páginas web. </p>
-                            <img src={money} className="valores" alt="discover descripcion ahorro"/>
-                       </Grid>
-                       <Grid item xs>
-                          <h3 className="home"> Encuentra tu producto favorito </h3>
-                          <p> Disfruta de una gran variedad de productos que se adapten a tus necesidades. </p>
-                          <img src={product} className="valores" alt="discover descripcion personas"/>
-                       </Grid>
-                   </Grid>
-                  </div>
-
-              </section>
-              <a id="upbutton"  href="/#"> <i className="fa fa-arrow-circle-up"></i></a>
-
+                <a href="#about">Acerca de</a>
+              </div>
+            </header>
+            <div id="slogan" className="centered">
+              <p>La aplicación que nadie pidió, pero que todos necesitan.</p>
+            </div>
           </div>
-        )
-    }
+        </section>
+        <section id="about" className="home">
+          <div id="info" className="gridcont">
+            <Grid container spacing={3}>
+              <Grid item xs={6}>
+                <h2 className="home"> ¿Quienes somos? </h2>
+                <p>Somos una plataforma web donde los usuarios pueden buscar cualquier tipo de producto que estén interesados en comprar. La plataforma recopila los productos buscados por el usuario en todas las tiendas, comparando precios y mostrando la mejor opción con el mejor precio, ahorrándole tiempo al cliente de tener que recurrir a buscar página por página mirando precios, Ez Browser lo hace automáticamente para tener una mejor Experiencia en compras.</p>
+              </Grid>
+              <Grid item xs={6} className="cuadro">
+                <img src={search} alt="cuadro quickmobility" />
+              </Grid>
+            </Grid>
+          </div>
+        </section>
+        <section id="ls" className="home">
+          <div id="otros" className="gridcont">
+            <Grid container spacing={3}>
+              <Grid item xs>
+                <h3 className="home"> Busca de forma más segura </h3>
+                <p> Podrás buscar tu producto sin necesidad de ingresar a distintas páginas web inseguras. </p>
+                <img src={secure} className="valores" alt="container descripcion" />
+              </Grid>
+              <Grid item xs>
+                <h3 className="home"> Ahorra Dinero </h3>
+                <p> Pagarás un valor más económico en comparación a otras páginas web. </p>
+                <img src={money} className="valores" alt="discover descripcion ahorro" />
+              </Grid>
+              <Grid item xs>
+                <h3 className="home"> Encuentra tu producto favorito </h3>
+                <p> Disfruta de una gran variedad de productos que se adapten a tus necesidades. </p>
+                <img src={product} className="valores" alt="discover descripcion personas" />
+              </Grid>
+            </Grid>
+          </div>
+
+        </section>
+        <a id="upbutton" href="/#"> <i className="fa fa-arrow-circle-up"></i></a>
+
+      </div>
+    )
+  }
 }
 
 
