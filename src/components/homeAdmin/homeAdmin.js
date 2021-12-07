98d1d9530e5f6e80e055cb4de9516c5b565d22b4
Carlos A <54123771+Carlos96999@users.noreply.github.com>
2021-12-06 19:27:34 -0500

    Vista adminHome

--- src/components/homeAdmin/homeAdmin.js
+++ src/components/homeAdmin/homeAdmin.js
@@ -1,14 +1,241 @@
-import React from 'react';
+import React, { useState, useMemo } from "react";
+import axios from 'axios';
+import './dashboard.css';
+import Flippy, { FrontSide, BackSide } from './lib';
 
-class loginAdmin extends React.Component 
-{
-    render(){
-    return (
-        <div>
-            Sitio principal del Sáenz
-        </div>
-    );
+const useSortableData = (items, config = null) => {
+  const [sortConfig, setSortConfig] = useState(config);
+
+  const sortedItems = useMemo(() => {
+    let sortableItems = [...items];
+    if (sortConfig !== null) {
+      sortableItems.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === "ascending" ? 1 : -1;
+        }
+        return 0;
+      });
     }
+    return sortableItems;
+  }, [items, sortConfig]);
+
+  const requestSort = key => {
+    let direction = "ascending";
+    if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === "ascending"
+    ) {
+      direction = "descending";
+    }
+    setSortConfig({ key, direction });
+  };
+
+  return { items: sortedItems, requestSort, sortConfig };
+};
+
+
+const FlippyStyle = {
+  width: '200px',
+  height: '300px',
+  textAlign: 'center',
+  color: '#FFF',
+  fontFamily: 'sans-serif',
+  fontSize: '30px',
+  justifyContent: 'center'
 }
 
-export default loginAdmin;
+
+const DefaultCardContents = ({ children }) => (
+  <React.Fragment>
+    <FrontSide
+      style={{
+        backgroundColor: '#41669d',
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column'
+      }}
+    >
+      Total de ventas en membresias
+      <span 
+        style={{
+          fontSize:'12px',
+          position: 'absolute',
+          bottom: '10px',
+          width: '100%'
+        }}>
+        {children}<br />
+      </span>
+    </FrontSide>
+    <BackSide
+      style={{
+        backgroundColor: '#175852',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column'
+      }}>
+      Cantidad de membresías vendidas: 4
+      Total vendido: $60.000
+      <span 
+        style={{
+          fontSize:'12px',
+          position: 'absolute',
+          bottom: '10px',
+          width: '100%'
+        }}>
+        {children}<br />
+      </span>
+    </BackSide>
+  </React.Fragment>);
+
+const FlippyOnHover = ({ flipDirection = 'vertical' }) => (
+  <Flippy
+    flipOnHover={true}
+    flipDirection={flipDirection}
+    style={FlippyStyle}
+  >
+    <DefaultCardContents>
+    
+    </DefaultCardContents>
+  </Flippy>
+);
+
+const ControlledFlippy = ({ isFlipped })  => (
+  <Flippy
+    flipDirection="vertical"
+    isFlipped={isFlipped}
+    style={FlippyStyle}
+  >
+  </Flippy>
+);
+
+function ProductTable(props) {
+  const { items, requestSort, sortConfig } = useSortableData(props.products);
+  const getClassNamesFor = name => {
+    if (!sortConfig) {
+      return;
+    }
+    return sortConfig.key === name ? sortConfig.direction : undefined;
+  };
+
+  return (
+    <div>
+      <table>
+      <caption>Ez Browser</caption>
+      <thead>
+        <tr>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort("name")}
+              className={getClassNamesFor("name")}
+            >
+              Membresía
+            </button>
+          </th>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort("price")}
+              className={getClassNamesFor("price")}
+            >
+              Precio
+            </button>
+          </th>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort("date")}
+              className={getClassNamesFor("date")}
+            >
+              Fecha de inicio
+            </button>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(product => (
+          <tr key={product.id}>
+            <td>{product.name}</td>
+            <td>{product.price}</td>
+            <td>{product.date}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    </div>
+  );
+}
+
+//export default function homeAdmin() {
+class homeAdmin extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFlipped: false
+    };
+  }
+
+
+  /*handleSubmit = async () => {
+    this.setState({ loginConfirm: false });
+    console.log(this.state.email);
+    console.log(this.state.password);
+    await axios.get('https://ezbrowser.herokuapp.com/ezpz/v1/membership',{
+    })
+        .then(async function (response) {
+            console.log(response);
+            /*if (response.data === true){
+                window.location.replace("https://ezbrowser-frontend.herokuapp.com/search");
+            }
+        }).catch(function (error){
+            console.log(error);
+        })
+    this.setState({ loginConfirm: true });
+  };*/
+  render(){
+  return (
+    <div className="container-homeAdmin">
+      <h1>
+        Admin
+      </h1>
+      <br/>
+      <div className="Card">
+        <div style={{ display: 'flex', flex: '1 0 200px', justifyContent: 'space-around', 'flex-wrap': 'wrap' }}>
+          <FlippyOnHover />
+          <ControlledFlippy
+            isFlipped={this.state.isFlipped}
+          /> 
+        </div>
+      </div>
+      <div >
+        <ProductTable
+          products={[
+            { id: 1, name: "El nombre de la membresía", price: "15.000", date: "1/12/2021" },
+            { id: 2, name: "El nombre de la membresía", price: "15.000", date: "31/12/2021" },
+            {
+              id: 3,
+              name: "El nombre de la membresía",
+              price: "15.000",
+              date: "11/12/2021"
+            },
+            {
+              id: 4,
+              name: "El nombre de la membresía",
+              price: "15.000",
+              date: "31/12/2021"
+            },
+            
+          ]}
+        />
+      </div>
+    </div>
+  );
+}
+}
+export default homeAdmin;
